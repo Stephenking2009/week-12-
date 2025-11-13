e438762945e9fcf29bc7b63b58583e0658a0732f
Stephenking2009 <sadelgado5@cps.edu>
2025-11-13 19:13:38 +0000

    notes

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -20,7 +20,23 @@ print(1 < x < 20)         # True
 
 
 # Practice Problems:
-
+#score ?grade checker
+#if the score is between 90 and 100 print A
+#if the score is between 80 and 90 print B
+#if the score is between 70 and 80 print C
+#if the score is between 60 and 70 print D
+grade = int(input("enter your score:"))
+if grade >= 100:
+    print("A")
+grade = int(input("enter your score:"))
+if grade >= 88:
+    print("B")
+    grade = int(input("enter your score:"))
+if grade >= 76:
+    print("C")
+    grade = int(input("enter your score:"))
+if grade >= 67:
+    print("D")
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
 
 # Write an expression that checks if a number is NOT equal to 0 and greater than 10.
