65dcab3e0eb25bb33433b5ac47c9e19fbf646acf
Stephenking2009 <sadelgado5@cps.edu>
2025-11-17 19:10:11 +0000

    list

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -6,19 +6,19 @@
 
 # Examples:
 
-my_list = ['apple', 'banana', 'cherry']
-print(my_list[0])         # apple
-print(my_list[1:])        # ['banana', 'cherry']
+#my_list = ['apple', 'banana', 'cherry']
+#print(my_list[0])         # apple
+#print(my_list[1:])        # ['banana', 'cherry']
 
-my_list.append('grape')
-print(my_list)
+#my_list.append('grape')
+#print(my_list)
 
-my_list.pop(1)
-print(my_list)
+#my_list.pop(1)
+#print(my_list)
 
-numbers = [3, 1, 4, 2]
-numbers.sort()
-print(numbers)
+#numbers = [3, 1, 4, 2]
+#numbers.sort()
+#print(numbers)
 
 
 # Practice Problems:
@@ -34,3 +34,41 @@ print(numbers)
 # Reverse your list using .reverse().
 
 # Create a list of 3 lists (matrix), and access the middle element.
+
+#collections are used to store multiple items in a single variable
+# list are ordered collections of items
+# lists are mutiple, meaning you can chnage their content
+# lists are created using square brackets
+list_of_fruits = ["apple", "bannana", "cherry", "date"]
+print(list_of_fruits) #['apple', 'banana', 'cherry','date']
+print(type(list_of_fruits))
+print(list_of_fruits[0])
+print(list_of_fruits[1])
+print(list_of_fruits[-1])
+print(list_of_fruits[1:3])
+list_of_fruits.reverse()
+print(list_of_fruits)
+print(list_of_fruits[::-1])
+list_of_fruits.append("elderberry")
+print(list_of_fruits)
+list_of_fruits.extend(["margo", "dragonfruit", "butterfly"])
+popped_item = list_of_fruits.pop()
+print(popped_item)
+print(list_of_fruits)
+list_of_fruits.insert(1, "blueberry")
+print(list_of_fruits)
+list_of_fruits.removal("bannana")
+print(list_of_fruits)
+list_of_fruits.insert(3, "bannana")
+list_of_fruits.sort()
+print(list_of_fruits)
+list_of_items = list(range(1, 101))
+print(list_of_items)
+list_of_items = list(range(1, 1001))
+print(list_of_items)
+list_of_items.extend(range(1001,2001))
+print(list_of_items)
+
+
+
+
